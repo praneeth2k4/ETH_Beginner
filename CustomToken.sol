47089d750cbcd5a25c8accb3bd503475cdba63ae
Praneeth.M <136630693+praneeth2k4@users.noreply.github.com>
2023-08-02 11:05:50 +0530

    Update CustomToken.sol

--- CustomToken.sol
+++ CustomToken.sol
@@ -1,19 +1,37 @@
 // SPDX-License-Identifier: MIT
-pragma solidity 0.8.18;
+pragma solidity ^0.8.18;
 
-contract CustomToken {
+/*
+       REQUIREMENTS
+    1. Your contract will have public variables that store the details about your coin (Token Name, Token Abbrv., Total Supply)
+    2. Your contract will have a mapping of addresses to balances (address => uint)
+    3. You will have a mint function that takes two parameters: an address and a value. 
+       The function then increases the total supply by that number and increases the balance 
+       of the “sender” address by that amount
+    4. Your contract will have a burn function, which works the opposite of the mint function, as it will destroy tokens. 
+       It will take an address and value just like the mint functions. It will then deduct the value from the total supply 
+       and from the balance of the “sender”.
+    5. Lastly, your burn function should have conditionals to make sure the balance of "sender" is greater than or equal 
+       to the amount that is supposed to be burned.
+*/
 
+contract MyToken {
+
+    //public varibales
     string public tokenName = "ZAPVI";
     string public tokenSymbol = "ZAP";
     uint public totalSupply = 0;
-
+    
+    //mapping variable
     mapping(address => uint) public tokenBalances;
 
+    //Mint Token Function
     function mintTokens(address _address, uint _amount) public {
         totalSupply += _amount;
         tokenBalances[_address] += _amount;
     }
 
+    //Burn token functions
     function burnTokens(address _address, uint _amount) public {
         require(tokenBalances[_address] >= _amount, "Insufficient balance");
         totalSupply -= _amount;
