28e53e928bc86d87c7b9309ee8b02c6a3b3234a6
Praneeth.M <136630693+praneeth2k4@users.noreply.github.com>
2023-08-02 10:54:31 +0530

    Updated CustomToken.sol

--- CustomToken.sol
+++ CustomToken.sol
@@ -1,22 +1,22 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.18;
 
-contract MyCustomToken {
+contract CustomToken {
 
-    string public tokenName = "MyCustomToken";
-    string public tokenSymbol = "MCT";
-    uint public totalTokenSupply = 0;
+    string public tokenName = "ZAPVI";
+    string public tokenSymbol = "ZAP";
+    uint public totalSupply = 0;
 
     mapping(address => uint) public tokenBalances;
 
     function mintTokens(address _address, uint _amount) public {
-        totalTokenSupply += _amount;
+        totalSupply += _amount;
         tokenBalances[_address] += _amount;
     }
 
     function burnTokens(address _address, uint _amount) public {
         require(tokenBalances[_address] >= _amount, "Insufficient balance");
-        totalTokenSupply -= _amount;
+        totalSupply -= _amount;
         tokenBalances[_address] -= _amount;
     }
 }
